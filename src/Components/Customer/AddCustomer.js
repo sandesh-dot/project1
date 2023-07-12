772a27768124fbb0a5983975cb94340ae581184a
Sandesh Koirala <sandeshkoirala1212@gmail.com>
2023-07-13 00:28:36 +0545

    Created Input Form

--- src/Components/Customer/AddCustomer.js
+++ src/Components/Customer/AddCustomer.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import style from './AddCustomer.module.css';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
 
-const AddUser=props=>{
-    
-};
+const AddCustomer=props=>{
+
+    const submitHandler=event=>{
+        event.preventDefault();
+    };
+
+    return(
+    <Card className={style.input}>
+        <form onSubmit={submitHandler}>
+            <label className={style.label} htmlFor='username'>Username</label>
+            <input id="username" type="text"></input>
+            <label htmlFor='age'>Age</label>
+            <input id="age" type="number"></input>
+            <Button type="submit">Register</Button>
+        </form>
+     </Card>
+    );
+}
+
+
 export default AddCustomer;
